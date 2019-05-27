9707a768a0b97d7b1652d6aa8683e0e876090a41
本田英太郎 <hondaeitaro@oolongtea.local>
2019-05-27 17:55:48 +0900

    sns login

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :exhibitors
+  has_many :buyers
+  has_many :credits
+
+  devise :omniauthable, omniauth_providers: %i[facebook google_oauth2]
+  # omniauthのコールバック時に呼ばれるメソッド
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+end
