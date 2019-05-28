92af86366a95ff8c9eae43986bfabb56f9af8080
本田英太郎 <hondaeitaro@oolongtea.local>
2019-05-28 17:19:50 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 # server "db.example.com", user: "deploy", roles: %w{db}
-server "(EC2のIPアドレス)", user: "ec2-user", roles: %w{app db web}
+server "52.198.197.180", user: "ec2-user", roles: %w{app db web}
 
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
